5b32ba3614cb3c7c1381b9ce7c4ca6ccd22f5cda
Shemil <shemmmil1996@gmail.com>
2020-04-04 10:42:43 +0300

    feat: add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+const Button = styled.button`
+  width: 100px;
+  height: 50px;
+  background-color: pink;
+  font-size: 18px;
+`;
+
+ReactDOM.render(<Button>Hello World</Button>, document.getElementById("root"));
