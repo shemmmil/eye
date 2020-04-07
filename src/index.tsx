0f1d033f20bec19bdfe0a7a4267ce4ed1bcd4964
Shemil <shemmmil1996@gmail.com>
2020-04-07 19:41:32 +0300

    feat: add style loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import 'antd/dist/antd.css';
 
 const Button = styled.button`
   width: 100px;
