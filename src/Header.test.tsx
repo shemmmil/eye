b6e7ef0a78cbaff73d2a855f7e146d09f40016b5
Shemil <shemmmil1996@gmail.com>
2020-04-09 19:42:41 +0300

    feat: add snapshot tests

--- src/Header.test.tsx
+++ src/Header.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import { Header } from './Header';
 
 describe('Header Render', () => {
   it('Middle navigation list items correctly renders', () => {
     const wrapper = mount(<Header />);
     expect(wrapper.find('li')).toHaveLength(5);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
