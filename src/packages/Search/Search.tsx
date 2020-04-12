1914071bd3f293e932dda84006acfd9e88e79f39
Shemil <shemmmil1996@gmail.com>
2020-04-12 13:09:25 +0300

    fix: remove packages folder

--- src/packages/Search/Search.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Input } from 'antd';
-
-const { Search: InputSearch } = Input;
-
-export const Search = () => {
-  return <InputSearch enterButton allowClear />;
-};
